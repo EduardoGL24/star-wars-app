cdbf5a619ea273418dd2101dcddd015c90a6acbc
eduardo <eduardo.gl24@hotmail.com>
2020-06-16 18:07:33 -0500

    add all images and add functions pagination

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -4,15 +4,12 @@ import './styles/Characters.scss';
 import Cards from '../components/Cards';
 
 export class Characters extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
+      state = {
           error: null,
           loading: false,
+          page: 1,
           items: []
-        };
-      }
+        }
 
       componentDidMount() {
         this.fetchData();
@@ -20,8 +17,9 @@ export class Characters extends Component {
 
       fetchData = async () => {
         this.setState({ loading: true, error: null });
+        console.log(this.state.page);
         try {
-          const data = await api.data.people(1);
+          const data = await api.data.people(this.state.page);
           this.setState({
             loading: false,
             items: data.results,
@@ -32,14 +30,30 @@ export class Characters extends Component {
         }
       }
 
+      nextPageImages(){
+        const {page} = this.state;
+        this.setState({ page: page + 1 },
+          () => this.fetchData())
+      }
+
+      previousPageImages(){
+        const {page} = this.state;
+        this.setState({page: page - 1 },
+          () => this.fetchData())
+      }
+
     render() {
       console.log(this.state.items)
         return (
             <React.Fragment>
-              <div className="characters-container">
+              <div id="startPage" className="characters-container">
                 <div className="characters-search-bar mb-5">
-                <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+                  <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
                 </div>
+                <div className="charters-pagination">
+                <button onClick={() => this.previousPageImages()} className="btn btn-primary">Anterior</button>
+                <button onClick={() => this.nextPageImages()} className="btn btn-primary">Siguiente</button>
+              </div>
                 <Cards items={this.state.items} images="characters"/>
               </div>
             </React.Fragment>
